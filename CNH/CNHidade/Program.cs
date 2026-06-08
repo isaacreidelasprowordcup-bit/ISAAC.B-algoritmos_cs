3375315b5025be13527245642cee1a4ddfedcff8
isaacreidelasprowordcup-bit <isaacreidelasprowordcup@gmail.com>
2026-06-08 11:48:30 -0300

    Add files via upload

---
+++ CNH/CNHidade/Program.cs
@@ -0,0 +1,21 @@
+﻿// Declaração das variáveis
+
+int anoNascimento = 0;
+int idade = 0;
+
+// Interação com o usuário
+
+Console.WriteLine("Informe o seu ano de nascimento: ");
+anoNascimento = Convert.ToInt16(Console.ReadLine());
+
+idade = 2026 - anoNascimento;
+if (idade >= 18)
+        {
+            Console.WriteLine("Voce ja pode tirar a CNH.");
+        }
+        else
+        {
+            Console.WriteLine("Voce ainda não pode tirar a CNH.");
+        }
+        
+
---
+++ NomeLadosTriangulos/NomeTriangulo/Program.cs
@@ -0,0 +1,29 @@
+﻿// Declaração de variáveis
+    {
+        double lado1, lado2, lado3;
+
+        Console.Write("Digite o primeiro lado: ");
+        lado1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o segundo lado: ");
+        lado2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o terceiro lado: ");
+        lado3 = Convert.ToDouble(Console.ReadLine());
+
+        if (lado1 == lado2 && lado2 == lado3)
+        {
+            Console.WriteLine(" O Triângulo é Equilatero!!! ");
+        }
+        else
+        {
+            if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+            {
+                Console.WriteLine(" O Triângulo é Isosceles!!! ");
+            }
+            else
+            {
+                Console.WriteLine(" O Triângulo é Escaleno!!! ");
+            }
+        }
+    }
