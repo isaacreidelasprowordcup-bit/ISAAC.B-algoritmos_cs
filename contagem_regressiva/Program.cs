777a6268ec677ed35dac8dff4360e219e779de7a
isaacreidelasprowordcup-bit <isaacreidelasprowordcup@gmail.com>
2026-06-10 16:23:09 -0300

    Add files via upload

---
+++ contagem_regressiva/Program.cs
@@ -0,0 +1,19 @@
+﻿// Algoritmo que faz uma contagem regressiva com o número seleionado
+
+double numero = 0.0;
+
+// Interação com o usuário
+
+Console.WriteLine("Digite seu número inteiro inicial: ");
+numero = Convert.ToDouble(Console.ReadLine());
+
+while (numero > 0)
+{
+    numero = numero - 1;
+    Console.WriteLine(numero);
+}
+{
+    Console.WriteLine("Fim da contagem regressiva!");
+}
+
+
