e3e6a9865c90000ed6c53008358b656aea4ed136
isaacreidelasprowordcup-bit <isaacreidelasprowordcup@gmail.com>
2026-06-10 16:25:39 -0300

    Add files via upload

---
+++ tabuada/Program.cs
@@ -0,0 +1,22 @@
+﻿// Algoritmo para exibir a tabuada de um número do 1 ao 10
+
+double num = 0.0;
+double multiplicador = 1;
+double resultado = num * multiplicador;
+
+// Interação com usuário
+
+Console.WriteLine("Insira um valor: ");
+num = Convert.ToDouble(Console.ReadLine());
+
+while (multiplicador <= 10)
+{
+    resultado = num * multiplicador;
+    Console.WriteLine($"{num} x {multiplicador} = {resultado}");
+    multiplicador++;
+}
+{
+    Console.WriteLine("fim da tabuada!");
+    Console.WriteLine("-----------");
+    Console.ReadLine();
+}
